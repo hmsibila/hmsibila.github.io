405f5df3cdf1dbff44bae5bf302b79f4d68ffa88
the publication <40344457+thepublication@users.noreply.github.com>
2020-01-02 16:55:41 -0800

    Update movearound.js

--- movearound.js
+++ movearound.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     animateDiv('.myButton');
-  /*  animateDiv('.b');
-    animateDiv('.c');
+ animateDiv('.b');
+     /*  animateDiv('.c');
     animateDiv('.d');*/
 });
 
